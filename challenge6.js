2a53c740c2f13c85de313e2d9c93e9bc5ba22bd4
Drew Bowling <andrewwbowling@gmail.com>
2023-03-03 14:24:00 -0700

    finish the assignment

--- challenge6.js
+++ challenge6.js
@@ -1,1 +1,19 @@
 //1. Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
+
+
+// var sentence = "turing is the best"
+// // charAt(0).to upper case
+
+var mySentence = "turing is the best"
+var words = mySentence.split(" ")
+
+//console.log(words)
+
+for (i = 0; i < words.length; i++) {
+     words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+}
+
+words.join(" ")
+
+console.log(words.toString())
+
